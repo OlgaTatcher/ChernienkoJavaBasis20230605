7571f1db308111499445d2adbc7f7deb04c6657a
OlgaTatcher <dai.akemi230318@gmail.com>
2023-06-06 08:07:15 +0300

    Second commit

--- Src/ChernienkoJavaBasis20230605.java
+++ Src/ChernienkoJavaBasis20230605.java
@@ -1,6 +1,8 @@
 public class ChernienkoJavaBasis20230605 {
     public static void main (String [] args){
         System.out.println("First line of homework");
+        System.out.println("Second line of homework");
+
 
     }
 
